199aa4c81881163852393e79d18599a9ae2241a7
shengguan13 <shengguan13@gmail.com>
2023-03-22 13:46:00 +0800

    Copy PurchaseIn to ProductionOrder, waiting modification

--- jshERP-web/src/views/bill/modules/ProductionOrderModal.vue
+++ jshERP-web/src/views/bill/modules/ProductionOrderModal.vue
@@ -50,21 +50,10 @@
             </a-form-item>
           </a-col>
           <a-col :lg="6" :md="12" :sm="24">
-            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="经手人" data-step="3" data-title="经手人"
-              data-intro="经手人的数据来自【经手人管理】菜单中的业务员">
-              <j-select-multiple placeholder="请选择经手人" v-model="personList.value" :options="personList.options"/>
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row class="form-row" :gutter="24">
-          <a-col :lg="6" :md="12" :sm="24">
-            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="计划开始日期">
-              <j-date v-decorator="['planStartTime', validatorRules.planStartTime]" :show-time="true"/>
-            </a-form-item>
-          </a-col>
-          <a-col :lg="6" :md="12" :sm="24">
-            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="计划完成日期">
-              <j-date v-decorator="['planFinishTime', validatorRules.planFinishTime]" :show-time="true"/>
+            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="关联生产计划" data-step="3" data-title="关联生产计划"
+              data-intro="生产单可以关联生产计划，选择之后会自动加载生产计划的内容，然后继续录入开工时间、数量等信息完成单据的提交，
+              提交之后原来的生产计划会对应的改变计划状态。">
+              <a-input-search placeholder="请选择生产计划" v-decorator="[ 'linkNumber' ]" @search="onSearchLinkNumber" :readOnly="true"/>
             </a-form-item>
           </a-col>
         </a-row>
@@ -76,22 +65,22 @@
           :minWidth="minWidth"
           :maxHeight="300"
           :rowNumber="false"
-          :rowSelection="true"
-          :actionButton="true"
-          :dragSort="true"
+          :rowSelection="rowCanEdit"
+          :actionButton="rowCanEdit"
+          :dragSort="rowCanEdit"
           @valueChange="onValueChange"
           @added="onAdded"
           @deleted="onDeleted">
           <template #buttonAfter>
-            <a-row :gutter="24" style="float:left;padding-bottom: 5px;" data-step="4" data-title="扫码录入" data-intro="此功能支持扫码枪扫描商品条码进行录入">
+            <a-row v-if="rowCanEdit" :gutter="24" style="float:left;padding-bottom: 5px;" data-step="4" data-title="扫码录入" data-intro="此功能支持扫码枪扫描商品条码进行录入">
               <a-col v-if="scanStatus" :md="6" :sm="24">
-                <a-button @click="scanEnter">扫码录入</a-button>
+                <a-button @click="scanEnter" style="margin-right: 8px">扫码录入</a-button>
               </a-col>
-              <a-col v-if="!scanStatus" :md="16" :sm="24" style="padding: 0 8px 0 12px">
+              <a-col v-if="!scanStatus" :md="16" :sm="24" style="padding: 0 6px 0 12px">
                 <a-input placeholder="请扫码商品条码并回车" v-model="scanBarCode" @pressEnter="scanPressEnter" ref="scanBarCode"/>
               </a-col>
-              <a-col v-if="!scanStatus" :md="6" :sm="24" style="padding: 0px 18px 0 0">
-                <a-button @click="stopScan">收起扫码</a-button>
+              <a-col v-if="!scanStatus" :md="6" :sm="24" style="padding: 0px 12px 0 0">
+                <a-button @click="stopScan" style="margin-right: 8px">收起扫码</a-button>
               </a-col>
             </a-row>
             <a-row :gutter="24" style="float:left;padding-bottom: 5px;">
@@ -110,7 +99,8 @@
         </a-row>
         <a-row class="form-row" :gutter="24">
           <a-col :lg="6" :md="12" :sm="24">
-            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="附件" data-step="5" data-title="附件" data-intro="可以上传与单据相关的图片、文档，支持多个文件">
+            <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="附件" data-step="11" data-title="附件"
+                         data-intro="可以上传与单据相关的图片、文档，支持多个文件">
               <j-upload v-model="fileList" bizPath="bill"></j-upload>
             </a-form-item>
           </a-col>
@@ -118,36 +108,39 @@
       </a-form>
     </a-spin>
     <many-account-modal ref="manyAccountModalForm" @ok="manyAccountModalFormOk"></many-account-modal>
+    <link-bill-list ref="linkBillList" @ok="linkBillListOk"></link-bill-list>
     <customer-modal ref="customerModalForm" @ok="customerModalFormOk"></customer-modal>
     <account-modal ref="accountModalForm" @ok="accountModalFormOk"></account-modal>
     <history-bill-list ref="historyBillListModalForm"></history-bill-list>
   </j-modal>
 </template>
+
 <script>
   import pick from 'lodash.pick'
   import ManyAccountModal from '../dialog/ManyAccountModal'
+  import LinkBillList from '../dialog/LinkBillList'
   import CustomerModal from '../../system/modules/CustomerModal'
   import AccountModal from '../../system/modules/AccountModal'
   import HistoryBillList from '../dialog/HistoryBillList'
   import { FormTypes } from '@/utils/JEditableTableUtil'
   import { JEditableTableMixin } from '@/mixins/JEditableTableMixin'
   import { BillModalMixin } from '../mixins/BillModalMixin'
-  import { getMpListShort,handleIntroJs } from "@/utils/util"
-  import JSelectMultiple from '@/components/jeecg/JSelectMultiple'
+  import { getMpListShort, changeListFmtMinus,handleIntroJs } from "@/utils/util"
+  import { getAction } from '@/api/manage'
   import JUpload from '@/components/jeecg/JUpload'
   import JDate from '@/components/jeecg/JDate'
   import Vue from 'vue'
   export default {
-    name: "ProductionPlanModal",
+    name: "ProductionOrderModal",
     mixins: [JEditableTableMixin, BillModalMixin],
     components: {
       ManyAccountModal,
+      LinkBillList,
       CustomerModal,
       AccountModal,
       HistoryBillList,
       JUpload,
       JDate,
-      JSelectMultiple,
       VNodes: {
         functional: true,
         render: (h, ctx) => ctx.props.vnodes,
@@ -162,10 +155,10 @@
         addDefaultRowNum: 1,
         visible: false,
         operTimeStr: '',
-        planStartTimeStr: '',
-        planFinishTimeStr: '',
-        prefixNo: 'XSDD',
+        prefixNo: 'CGRK',
+        depositStatus: false,
         fileList:[],
+        rowCanEdit: true,
         model: {},
         labelCol: {
           xs: { span: 24 },
@@ -191,35 +184,27 @@
             { title: '扩展信息', key: 'materialOther', width: '5%', type: FormTypes.normal },
             { title: '库存', key: 'stock', width: '5%', type: FormTypes.normal },
             { title: '单位', key: 'unit', width: '4%', type: FormTypes.normal },
-            { title: '数量', key: 'operNumber', width: '5%', type: FormTypes.inputNumber, statistics: true,
+            { title: '原计划', key: 'preNumber', width: '4%', type: FormTypes.normal },
+            { title: '已生产', key: 'finishNumber', width: '4%', type: FormTypes.normal },
+            { title: '数量', key: 'operNumber', width: '4%', type: FormTypes.inputNumber, statistics: true,
               validateRules: [{ required: true, message: '${title}不能为空' }]
             },
-            { title: '备注', key: 'remark', width: '6%', type: FormTypes.input }
+            { title: '备注', key: 'remark', width: '6%', type: FormTypes.input },
+            { title: '关联id', key: 'linkId', width: '5%', type: FormTypes.hidden },
           ]
         },
         confirmLoading: false,
         validatorRules:{
           operTime:{
             rules: [
-              { required: true, message: '请输入单据日期!' }
-            ]
-          },
-          // TODO: 需要确保完成日期晚于开始日期
-          planStartTime:{
-            rules: [
-              { required: true, message: '请输入计划开始日期!' }
-            ]
-          },
-          planFinishTime:{
-            rules: [
-              { required: true, message: '请输入计划完成日期!' }
+              { required: true, message: '请输入单据日期！' }
             ]
           },
           organId:{
             rules: [
-              { required: true, message: '请选择客户!' }
+              { required: true, message: '请选择客户！' }
             ]
-          }
+          },
         },
         url: {
           add: '/depotHead/addDepotHeadAndDetail',
@@ -234,23 +219,27 @@
       //调用完edit()方法之后会自动调用此方法
       editAfter() {
         this.billStatus = '0'
+        this.rowCanEdit = true
+        this.materialTable.columns[1].type = FormTypes.popupJsh
         this.changeColumnHide()
+        this.changeFormTypes(this.materialTable.columns, 'preNumber', 0)
+        this.changeFormTypes(this.materialTable.columns, 'finishNumber', 0)
         if (this.action === 'add') {
+          this.depositStatus = false
           this.addInit(this.prefixNo)
-          this.personList.value = ''
           this.fileList = []
           this.$nextTick(() => {
             handleIntroJs(this.prefixNo, 1)
           })
         } else {
+          if(this.model.linkNumber) {
+            this.rowCanEdit = false
+            this.materialTable.columns[1].type = FormTypes.normal
+          }
           this.model.operTime = this.model.operTimeStr
-          this.model.planStartTime = this.model.planStartTimeStr
-          this.model.planFinishTime = this.model.planFinishTimeStr
-          this.personList.value = this.model.salesMan
           this.fileList = this.model.fileName
           this.$nextTick(() => {
-            this.form.setFieldsValue(pick(this.model,'organId',
-              'operTime', 'planStartTime', 'planFinishTime', 'number', 'remark', 'salesMan'))
+            this.form.setFieldsValue(pick(this.model,'organId', 'operTime', 'number', 'linkNumber', 'remark'))
           });
           // 加载子表数据
           let params = {
@@ -269,19 +258,20 @@
         }
         this.initSystemConfig()
         this.initCustomer()
-        this.initSalesman()
         this.initAccount()
       },
       //提交单据时整理成formData
       classifyIntoFormData(allValues) {
-      // TODO: 可以手动设置一些field为0，在设置新的type之后，如果可以和销售、采购分开，则不需要
         let billMain = Object.assign(this.model, allValues.formValue)
         let detailArr = allValues.tablesValue[0].values
-        billMain.type = '其它'
-        billMain.subType = '销售订单'
+        billMain.type = '入库'
+        billMain.subType = '采购'
         billMain.defaultNumber = billMain.number
         billMain.totalPrice = 0
-        billMain.accountId = ''
+        billMain.changeAmount = 0
+        if(billMain.accountId === 0) {
+          billMain.accountId = ''
+        }
         billMain.accountIdList = ""
         billMain.accountMoneyList = ""
         if(this.fileList && this.fileList.length > 0) {
@@ -292,7 +282,6 @@
         if(this.model.id){
           billMain.id = this.model.id
         }
-        billMain.salesMan = this.personList.value
         billMain.status = this.billStatus
         return {
           info: JSON.stringify(billMain),
@@ -301,8 +290,38 @@
       },
       handleHistoryBillList() {
         let organId = this.form.getFieldValue('organId')
-        this.$refs.historyBillListModalForm.show('其它', '销售订单', '客户', organId);
+        this.$refs.historyBillListModalForm.show('入库', '采购', '供应商', organId);
         this.$refs.historyBillListModalForm.disableSubmit = false;
+      },
+      onSearchLinkNumber() {
+        this.$refs.linkBillList.show('其它', '采购订单', '供应商', "1,3")
+        this.$refs.linkBillList.title = "选择生产计划"
+      },
+      linkBillListOk(selectBillDetailRows, linkNumber, organId, discountMoney, deposit, remark) {
+        this.rowCanEdit = false
+        this.materialTable.columns[1].type = FormTypes.normal
+        this.changeFormTypes(this.materialTable.columns, 'preNumber', 1)
+        this.changeFormTypes(this.materialTable.columns, 'finishNumber', 1)
+        if(selectBillDetailRows && selectBillDetailRows.length>0) {
+          let listEx = []
+          for(let j=0; j<selectBillDetailRows.length; j++) {
+            let info = selectBillDetailRows[j];
+            if(info.finishNumber>0) {
+              info.operNumber = info.preNumber - info.finishNumber
+            }
+            info.linkId = info.id
+            listEx.push(info)
+            this.changeColumnShow(info)
+          }
+          this.materialTable.dataSource = listEx
+          this.$nextTick(() => {
+            this.form.setFieldsValue({
+              'organId': organId,
+              'linkNumber': linkNumber,
+              'remark': remark
+            })
+          })
+        }
       },
     }
   }
